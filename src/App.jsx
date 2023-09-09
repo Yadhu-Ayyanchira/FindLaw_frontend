c5c1c0321caf80f1cd2f959d44ef6aaf016ecf56
Yadhu <yadhukrishnanas@gmail.com>
2023-09-09 16:20:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>hey there</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+    },
+  },
+  plugins: [],
+});
