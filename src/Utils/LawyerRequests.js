910af60caa5fa6ca18ad9723ff1853dd1bd5bf45
Yadhu <yadhukrishnanas@gmail.com>
2023-10-23 22:45:46 +0530

    lawyer auth

--- src/Utils/LawyerRequests.js
+++ src/Utils/LawyerRequests.js
@@ -8,17 +8,33 @@ LawyerRequest.interceptors.request.use((req) => {
   if (localStorage.getItem("currentLawyer")) {
     console.log("setting head");
     req.headers.Authorization =
-      "Bearer " + localStorage.getItem("currentLawyer");
+       localStorage.getItem("currentLawyer");
   }
   return req;
 });
 
-LawyerRequest.interceptors.response.use((response) => {
-  if (response?.data?.message == "User Blocked") {
-    localStorage.removeItem("currentLawyer");
-    window.location.href = "/lawyer/login";
+// LawyerRequest.interceptors.response.use((response) => {
+//   if (response?.data?.message == "User Blocked") {
+//     localStorage.removeItem("currentLawyer");
+//     window.location.href = "/lawyer/login";
+//   }
+//    return response;
+// });
+LawyerRequest.interceptors.response.use(
+  (response) => {
+    if (response?.data?.message === "User Blocked") {
+      localStorage.removeItem("currentLawyer");
+      window.location.href = "/lawyer/login";
+    }
+    return response;
+  },
+  (error) => {
+    console.error("Request error:", error);
+    if (error.response && error.response.status === 400) {
+      window.location.href = "/lawyer/login";
+    }
+    return Promise.reject(error);
   }
-   return response;
-});
+);
 
 export default LawyerRequest;
