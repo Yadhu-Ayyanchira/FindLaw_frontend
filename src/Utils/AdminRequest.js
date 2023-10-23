1dcdf6399328ae77a7282ddf84e7a013d8acb335
Yadhu <yadhukrishnanas@gmail.com>
2023-10-23 23:00:51 +0530

    lawyer,admin rolebased auth

--- src/Utils/AdminRequest.js
+++ src/Utils/AdminRequest.js
@@ -5,8 +5,7 @@ const AdminRequest = axios.create({
 
 AdminRequest.interceptors.request.use((req) => {
   if (localStorage.getItem("currentAdmin")) {
-    console.log("have curr admin");
-    req.headers.Authorization = "Bearer " + localStorage.getItem("currentAdmin");
+    req.headers.Authorization = localStorage.getItem("currentAdmin");
   }
   return req;
 });
