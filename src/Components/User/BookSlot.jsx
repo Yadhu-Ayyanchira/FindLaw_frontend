e238042cccb972e3c9d017fc5c9775d4598dc599
Yadhu <yadhukrishnanas@gmail.com>
2023-10-30 12:05:55 +0530

    BOOKING DESIGN

--- src/Components/User/BookSlot.jsx
+++ src/Components/User/BookSlot.jsx
@@ -14,6 +14,7 @@ import { useQuery } from "@tanstack/react-query";
 import moment from "moment";
 import Loader from "../Loader/Loader";
 import { useState } from "react";
+import Booking from "./Booking";
 
 function BookSlot() {
   const location = useLocation();
@@ -43,7 +44,9 @@ function BookSlot() {
   if (dateError ) {
     return <div>Error: {dateError.message}</div>;
   }
- 
+ const handleBooking = ()=>{
+  setBooking(!booking)
+ }
 
   return (
     <>
@@ -98,6 +101,8 @@ function BookSlot() {
                 label="Choose date"
                 value={selectedDate}
                 onChange={(newSelectedDate) => {
+                  // handleBooking();s
+                  setBooking(false)
                   setSelectedDate(newSelectedDate);
                 }}
               >
@@ -115,58 +120,70 @@ function BookSlot() {
               <AddSlot />
             </div> */}
           </div>
-          <Card className=" shadow-2xl m-5 p-5 grid grid-cols-3 gap-x-8">
-            {slotData ? (
-              slotData.data.map((slot, i) => (
-                <Card
-                  key={i}
-                  className="mt-6  mx-2 min-w-min bg-blue-gray-50/30 rounded-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1"
-                >
-                  <CardBody>
-                    <Typography
-                      variant="h4"
-                      color={slot.isBooked == true ? "red" : "green"}
-                      className="mb-2 font-serif"
+          {booking ? (
+            <>
+            <Booking/>
+            </>
+          ) : (
+            <>
+              <Card className=" shadow-2xl m-5 p-5 grid grid-cols-3 gap-x-8">
+                {slotData ? (
+                  slotData.data.map((slot, i) => (
+                    <Card
+                      key={i}
+                      className="mt-6  mx-2 min-w-min bg-blue-gray-50/30 rounded-md hover:shadow-lg transition duration-300 ease-in-out transform hover:-translate-y-1"
                     >
-                      {slot.isBooked == true ? "Not available" : "Available"}
-                    </Typography>
-                    <div>
-                      <Typography>
-                        <span className="font-serif text-xl">Time:</span>
-                        <span className="text-green-700 font-bold">
-                          {slot.slotTime}
-                        </span>
-                      </Typography>
-                      <Typography>
-                        <span className="font-serif text-xl">Date:</span>
-                        <span className="text-green-700 font-bold">
-                          {new Date(slot.slotDate).toLocaleDateString("en-GB")}
-                        </span>
-                      </Typography>
-                    </div>
-                    <Typography color="gray">
-                      maximum of 30 minutes
-                      <span className="font-serif text-blue-700">*</span>
-                    </Typography>
-                  </CardBody>
-                  <CardFooter className="pt-0">
-                    {slot.isBooked == false ? (
-                      <button className="flex flex-row rounded-lg border-2 border-blue-500 px-5 py-1 text-base font-medium text-blue-500 transition duration-200 hover:bg-blue-100 active:bg-green-700/5 dark:border-green-400 dark:bg-green-400/10 dark:text-white dark:hover:bg-green-300/10 dark:active:bg-green-200/10">
-                        <BookmarkSquareIcon className="w-6 me-2" />
-                        Book now
-                      </button>
-                    ) : (
-                      <Typography variant="h5" className="text-red-700">
-                        Booked
-                      </Typography>
-                    )}
-                  </CardFooter>
-                </Card>
-              ))
-            ) : (
-              <h1>Select a date to show available slots</h1>
-            )}
-          </Card>
+                      <CardBody>
+                        <Typography
+                          variant="h4"
+                          color={slot.isBooked == true ? "red" : "green"}
+                          className="mb-2 font-serif"
+                        >
+                          {slot.isBooked == true
+                            ? "Not available"
+                            : "Available"}
+                        </Typography>
+                        <div>
+                          <Typography>
+                            <span className="font-serif text-xl">Time:</span>
+                            <span className="text-green-700 font-bold">
+                              {slot.slotTime}
+                            </span>
+                          </Typography>
+                          <Typography>
+                            <span className="font-serif text-xl">Date:</span>
+                            <span className="text-green-700 font-bold">
+                              {new Date(slot.slotDate).toLocaleDateString(
+                                "en-GB"
+                              )}
+                            </span>
+                          </Typography>
+                        </div>
+                        <Typography color="gray">
+                          maximum of 30 minutes
+                          <span className="font-serif text-blue-700">*</span>
+                        </Typography>
+                      </CardBody>
+                      <CardFooter className="pt-0">
+                        {slot.isBooked == false ? (
+                          <button onClick={handleBooking} className="flex flex-row rounded-lg border-2 border-blue-500 px-5 py-1 text-base font-medium text-blue-500 transition duration-200 hover:bg-blue-100 active:bg-green-700/5 dark:border-green-400 dark:bg-green-400/10 dark:text-white dark:hover:bg-green-300/10 dark:active:bg-green-200/10">
+                            <BookmarkSquareIcon className="w-6 me-2" />
+                            Book now
+                          </button>
+                        ) : (
+                          <Typography variant="h5" className="text-red-700">
+                            Booked
+                          </Typography>
+                        )}
+                      </CardFooter>
+                    </Card>
+                  ))
+                ) : (
+                  <h1>Select a date to show available slots</h1>
+                )}
+              </Card>
+            </>
+          )}
         </div>
       </div>
     </>
--- src/Components/User/Booking.jsx
+++ src/Components/User/Booking.jsx
@@ -1,7 +1,51 @@
+import { Card, CardBody, Typography } from "@material-tailwind/react";
+import { useSelector } from "react-redux"
+import nocredit from "../../Assets/Images/nocredit.jpg"
+import touch from "../../Assets/Images/touch.jpg"
+import { useNavigate } from "react-router-dom";
+import { EnvelopeIcon, GlobeAltIcon } from "@heroicons/react/24/solid";
+
+
 function Booking() {
+  const {flc} = useSelector((state)=>state.user)
+  const navigate = useNavigate()
   return (
-    
-  )
+    <>
+      {flc < 300 ? (
+        <>
+          <Card className="grid place-items-center shadow-2xl m-5 p-5 gap-x-8">
+            <div className="text-center grid place-items-center">
+              <img src={nocredit} alt="" className="w-72" />
+              <Typography variant="h5">
+                You need to have at least 300 FLCs in your account to book a
+                room!
+              </Typography>
+              <button
+                onClick={() => navigate("/userProfile")}
+                className="outline-green-700 outline w-32 rounded-xl"
+              >
+                ADD FLC NOW
+              </button>
+            </div>
+          </Card>
+        </>
+      ) : (
+        <>
+          <Card className="grid place-items-center shadow-2xl m-5 p-5 gap-x-8">
+            <div className="text-center grid place-items-center">
+              <img src={touch} alt="" className="w-60" />
+
+              <div className="flex flex-row justify-evenly pt-3">
+                <button className="flex flex-row rounded-lg border-2 border-green-500 px-5 py-1 text-base font-medium text-green-500 transition duration-200 hover:bg-green-100 active:bg-green-700/5 dark:border-green-400 dark:bg-green-400/10 dark:text-white dark:hover:bg-green-300/10 dark:active:bg-green-200/10">
+                  BOOK NOW
+                </button>
+              </div>
+            </div>
+          </Card>
+        </>
+      )}
+    </>
+  );
 }
 
 export default Booking
